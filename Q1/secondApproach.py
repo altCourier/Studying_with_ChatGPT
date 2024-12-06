f67f50c56c1ed8ff2c5b0ba639c69eeb45a3b722
altCourier <szehra_uluyol@outlook.com>
2024-12-06 18:42:33 +0300

    Update secondApproach.py

--- Q1/secondApproach.py
+++ Q1/secondApproach.py
@@ -1,6 +1,6 @@
 # If we XOR two same values A XOR A =  0
 # So if we just XOR elements starting from 1 to n
-# The num that doesnt get 0 is the missing value.
+# We can find the missing value.
 
 arr = [1,2,3,4,6,7,8]
 def missingNumber(arr, n):
